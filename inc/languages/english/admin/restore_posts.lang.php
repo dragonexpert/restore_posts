fd4f394d9fbf0a1613e358a65b8dbda599e4bd2d
dragonexpert <arceus@teamdimensional.net>
2019-02-23 07:51:19 -0600

    Create restore_posts.lang.php

---
+++ inc/languages/english/admin/restore_posts.lang.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Latios
+ * Date: 2/23/2019
+ * Time: 7:35 AM
+ */
+if(!defined("IN_MYBB"))
+{
+    die("Direct access not allowed.");
+}
+
+$l['restore_posts_title'] = "Restore Posts &amp; Threads";
+$l['restore_posts_description'] = "Allows you to manage soft deleted content from the Mod CP.";
+$l['restore_posts_description_paypal'] = "<form action=\"https://www.paypal.com/cgi-bin/webscr\" method=\"post\" target=\"_top\">
+<input type=\"hidden\" name=\"cmd\" value=\"_donations\" />
+<input type=\"hidden\" name=\"business\" value=\"N894YGCV236J8\" />
+<input type=\"hidden\" name=\"currency_code\" value=\"USD\" />
+<input type=\"image\" src=\"https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif\" border=\"0\" name=\"submit\" title=\"PayPal - The safer, easier way to pay online!\" alt=\"Donate with PayPal button\" />
+<img alt=\"\" border=\"0\" src=\"https://www.paypal.com/en_US/i/scr/pixel.gif\" width=\"1\" height=\"1\" />
+</form>
+";
