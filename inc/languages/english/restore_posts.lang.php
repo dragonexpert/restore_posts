df7cca06cb14e230433b97ae3bd62f7d6c5a0493
dragonexpert <arceus@teamdimensional.net>
2019-02-23 07:33:18 -0600

    Create restore_posts.lang.php

---
+++ inc/languages/english/restore_posts.lang.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Latios
+ * Date: 2/22/2019
+ * Time: 9:53 AM
+ */
+if(!defined("IN_MYBB"))
+{
+    die("Direct access not allowed.");
+}
+$l['restore_posts_redirect'] = "The actions have been performed.";
+$l['restore_posts_restore_posts'] = "Restored posts";
+$l['restore_posts_restore'] = "Restore";
+$l['restore_posts_posts'] = "Restore Posts";
+$l['restore_posts_restore_threads'] = "Restore Threads";
+$l['restore_posts_threads'] = "Threads";
+$l['restore_posts_post_link'] = "Posts";
+$l['restore_posts_threads_queue_empty'] = "There are no soft deleted threads.";
+$l['restore_posts_posts_queue_empty'] = "There are no soft deleted posts.";
